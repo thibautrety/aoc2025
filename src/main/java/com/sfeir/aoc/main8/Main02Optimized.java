ee3882a82460f5d12d4672fa0de6c2862ff5d620
thibautrety <thibaut.rety@gmail.com>
2025-12-08 11:09:04 +0100

    Day 8 optimization

--- src/main/java/com/sfeir/aoc/main8/Main02Optimized.java
+++ src/main/java/com/sfeir/aoc/main8/Main02Optimized.java
@@ -5,8 +5,6 @@ import com.sfeir.aoc.graph.Graph;
 import com.sfeir.aoc.utils.Connection;
 import com.sfeir.aoc.utils.ReadFile;
 import com.sfeir.aoc.utils.Triple;
-import java.util.Map;
-import java.util.Set;
 
 public class Main02Optimized {
     public static void main() {
@@ -37,58 +35,6 @@ public class Main02Optimized {
         }
         System.out.println(connection.a().a()*connection.b().a());
     }
-
-    private static void removeKey(Triple maxKey, Map<Triple, Set<Triple>> linkedTriples) {
-        var ToRemove = linkedTriples.get(maxKey);
-        ToRemove.forEach(linkedTriples::remove);
-        linkedTriples.remove(maxKey);
-    }
-
-    private static Triple findMax(Map<Triple, Set<Triple>> linkedTriples) {
-        Triple result = null;
-        int maxSize = Integer.MIN_VALUE;
-        for (var entry : linkedTriples.entrySet()) {
-            if (entry.getValue().size() > maxSize) {
-                maxSize = entry.getValue().size();
-                result = entry.getKey();
-            }
-        }
-        return result;
-    }
-
-    private static Connection findShortest(Set<Triple> triples, Set<Connection> connections) {
-        Connection result = null;
-        double minDistance = Double.MAX_VALUE;
-        for (Triple t1 : triples) {
-            for (Triple t2 : triples) {
-                if (!t1.equals(t2) && !connections.contains(new Connection(t1, t2)) && !connections.contains(new Connection(t2, t1))) {
-                    var currentDistance = t1.distance(t2);
-                    if (currentDistance < minDistance) {
-                        minDistance = currentDistance;
-                        result = new Connection(t1, t2);
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    private static Triple min(Set<Triple> triples, Triple source, Set<Triple> excluded) {
-        double minDistance = Double.MAX_VALUE;
-        Triple result = null;
-        for (Triple t : triples) {
-            if (!t.equals(source) && !excluded.contains(t)) {
-                var current = t.distance(source);
-                if (current < minDistance) {
-                    minDistance = current;
-                    result = t;
-                }
-            }
-        }
-
-        return result;
-    }
-
 }
 
 
